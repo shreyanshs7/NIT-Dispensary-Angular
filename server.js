422d964be63b044c3e985df805571cbe7981bfc0
shreyanshs7 <shreyanshss7@gmail.com>
2018-03-03 13:46:12 +0530

    Updated package.json file added cors in server.js

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const path = require('path');
+const cors = require('express-cors');
 // Serve only the static files form the dist directory
 app.use(express.static(path.join(__dirname) + '/dist'));
+
+app.use(cors({
+    allowedOrigins: [
+        'http://localhost:4200/', 'https://nitrr-dispensary.herokuapp.com'
+    ]
+}));
 
 // Start the app by listening on the default Heroku port
 app.get("*",(req,res)=>{
