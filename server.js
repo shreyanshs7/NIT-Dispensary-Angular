a6a91b615d47b3c7a4390789ed65171be3b2e8f1
shreyanshs7 <shreyanshss7@gmail.com>
2018-03-03 14:18:05 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const path = require('path');
-const cors = require('express-cors');
+const cors = require('cors');
 // Serve only the static files form the dist directory
 app.use(express.static(path.join(__dirname) + '/dist'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
     });
+
+var originsList = [
+    'https://nitrr-dispensary.herokuapp.com','http://localhost:4200'
+]
+
+app.use(cors({
+    origin : function(origin, callback){
+        var isWhitelisted = originsList.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+  },
+  credentials:true
+}));
 
 // Start the app by listening on the default Heroku port
 app.get("*",(req,res)=>{
